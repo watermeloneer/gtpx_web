c77c1657f004b2f2e928d7a1d48aa19f863105d4
zdzMAC <duanzhengzhang@gmail.com>
2017-07-09 19:02:17 +0800

    前端修改

--- static/classic/js/common.js
+++ static/classic/js/common.js
@@ -9,109 +9,6 @@ $(function () {
     chapterList();
     submitInfo();
     getChapterNumber();
-    function chapterListtest() {
-        var response = {
-            "count": 10,
-            "next": null,
-            "previous": null,
-            "results": [
-                {
-                    "num": 0,
-                    "name": "安全操作技术（图片）"
-                },
-                {
-                    "num": 1,
-                    "name": "叉车部件识别（图片）"
-                },
-                {
-                    "num": 2,
-                    "name": "叉车零部件（图片）"
-                },
-                {
-                    "num": 3,
-                    "name": "法规与标准知识"
-                },
-                {
-                    "num": 4,
-                    "name": "基础知识"
-                },
-                {
-                    "num": 5,
-                    "name": "交通标志（图片）"
-                },
-                {
-                    "num": 6,
-                    "name": "仪表识别（图片）"
-                },
-                {
-                    "num": 7,
-                    "name": "安全法知识"
-                },
-                {
-                    "num": 8,
-                    "name": "专业知识"
-                },
-                {
-                    "num": 9,
-                    "name": "安全知识"
-                }
-            ]
-        };
-        var chapterlist = response.results;
-        for(var i=0;i<chapterlist.length;i++){
-            $("#chapterlist").append('<option value="'+ chapterlist[i].num +'">'+ chapterlist[i].name+'</option>');
-        }
-        $('#chapterlist option:first').attr('selected','selected');
-    }
-
-
-
-
-    function submitInfotest() {
-                    var resopnse = {
-                        "count": 24,
-                        "next": "http://192.168.0.103:8000/problem/list/?category=1&chapter=1&page=2",
-                        "previous": null,
-                        "results": [
-                            {
-                                "course": "叉车司机(N2)",
-                                "chapter": "安全操作技术（图片）",
-                                "title": "2、根据以下现象选择正确的观点？(单选题)",
-                                "choices": [
-                                    "A、车入车库需手动开门",
-                                    "B、车辆检修需停稳",
-                                    "C、检修时，叉齿需降至地面",
-                                    "D、检修需关闭发动机"
-                                ],
-                                "answers": "A",
-                                "images": "http://192.168.0.103:8000/static/upload/yhpicture/nj_cz_t2_tp97.jpg",
-                                "category": "单选题"
-                            }
-                        ]
-                    }
-                    var imgSrc = resopnse.results[0].images;
-                    var Option = resopnse.results[0].choices;
-                    var answer = resopnse.results[0].answers;
-                    nextTitle = resopnse.next;
-                    previousTitle = resopnse.previous;
-                    count = resopnse.count;
-                    if(nextTitle != null){
-                        categoryNum = GetRequesttitle(nextTitle);
-                        var titleNum = GetRequest(nextTitle) - 1;
-                    }else{
-                        categoryNum = GetRequesttitle(previousTitle);
-                        var titleNum = GetRequest(previousTitle) + 1;
-                    }
-                    $('#titleImage').attr('src',imgSrc);
-                    $("#titleOption").append('');
-                    for(var i = 0; i<Option.length; i++){
-                        $("#titleOption").append('<li class="clearfix"><span>'+ Option[i] +'</span></li>');
-                    }
-
-                    $('#answer').text('正确答案：'+ answer);
-                    $('#titleNum').text('第 ' + titleNum + ' 题：');
-
-    }
 
 })
 
@@ -201,6 +98,7 @@ $(function () {
             url:"/problem/chapters/list",
             success: function (response) {
                 var chapterlist = response.results;
+                $("#chapterlist").append('');
                 for(var i=0;i<chapterlist.length;i++){
                     $("#chapterlist").append('<option value="'+ i +'">'+ chapterlist[i]+'</option>');
                 }

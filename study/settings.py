6cb17ffd706d8ef54486c2a93f97d2c7a1aef0dd
turkey <zaihuazhao@163.com>
2017-07-20 21:20:44 +0800

    add cached backend for sessions

--- study/settings.py
+++ study/settings.py
@@ -166,7 +166,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 20,
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
     ),

8a5f32fd0d2c830f0fee91f78609b0728f264606
turkey <zaihuazhao@163.com>
2017-08-07 22:18:18 +0800

    修改 ProblemTemp, ChapterTemp 模型， 新增level字段表示题库来源

--- apps/problems/models.py
+++ apps/problems/models.py
@@ -69,6 +69,10 @@ class ProbelmTemp(models.Model):
         (2, '多选题'),
         (3, '图片题'),
     )
+    LEVEL_CHOICES = (
+        (0, '全国'),
+        (1, '省级')
+    )
     course = models.IntegerField('所属课程id')
     chapter= models.IntegerField('所属章节id')
     num = models.IntegerField('序号', blank=True, null=True)
@@ -77,13 +81,14 @@ class ProbelmTemp(models.Model):
     answers = models.CharField('答案', max_length=20, blank=True)
     images = models.ImageField('图片', upload_to='yhpicture', max_length=200, blank=True)
     category = models.IntegerField('题目类型', choices=CATEGORY_TYPES, default=0)
+    level = models.IntegerField('题目级别', choices=LEVEL_CHOICES, default=0)
 
     class Meta:
         db_table = 'problems_temp'
         verbose_name = verbose_name_plural = '题目'
 
     def __str__(self):
-        return '项目:%s 知识模块:%s 记录:%s' % (self.course, self.chapter, self.title)
+        return '项目:%s 知识模块:%s 记录:%s，级别:%s' % (self.course, self.chapter, self.title, self.get_level_display())
 
 
 class ChapterTemp(models.Model):
@@ -92,17 +97,21 @@ class ChapterTemp(models.Model):
     #     "group": 0,
     #     "name": "返回大纲明细"
     # }
-
+    LEVEL_CHOICES = (
+        (0, '全国'),
+        (1, '省级')
+    )
     num = models.IntegerField('知识模块id')
     course = models.IntegerField('所属项目num')
     name = models.CharField('知识模块', max_length=100)
+    level = models.IntegerField('题目级别', choices=LEVEL_CHOICES, default=0)
 
     class Meta:
         db_table = 'chapters_temp'
         verbose_name = verbose_name_plural = '知识模块'
 
     def __str__(self):
-        return '项目:%s 记录:%s：%s' % (self.course, self.num, self.name)
+        return '项目:%s 记录:%s：%s, 级别: %s' % (self.course, self.num, self.name, self.get_level_display())
 
 
 class CourseTemp(models.Model):

a578eee4168b0fb6d2f23164dd15e410e5a91091
turkey <zaihuazhao@163.com>
2017-08-08 01:07:01 +0800

    修改 选择题目类型链接

--- apps/problems/views.py
+++ apps/problems/views.py
@@ -1,3 +1,13 @@
+# _*_ coding:utf-8 _*_
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
+
+@login_required
+def problems_view(request):
+    """做题页面"""
+
+    return render(request, 'frontpage.html')
+
+

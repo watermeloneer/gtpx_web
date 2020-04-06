007669c9ff4a81e63e0eebc3f23fef84157cab5d
Frankie <zaihuazhao@163.com>
2020-04-06 12:07:22 +0800

    fix

--- apps/exams/views.py
+++ apps/exams/views.py
@@ -50,10 +50,11 @@ def create_exam(request):
 
 
 
-def gen_problems_str(course, level):
+def gen_problems_str(course, level=None):
     """产生题目列表"""
+    # ids_list = ProbelmTemp.objects.filter(course=course, level=level).exclude(category=4).only('pk').values_list('pk', flat=True)
 
-    ids_list = ProbelmTemp.objects.filter(course=course, level=level).exclude(category=4).only('pk').values_list('pk', flat=True)
+    ids_list = ProbelmTemp.objects.filter(course=course).exclude(category=4).only('pk').values_list('pk', flat=True)
     ids_list = list(ids_list)
     selected_ids = random.sample(ids_list, 100 if len(ids_list) > 100 else len(ids_list) - 5)
     string = ''
